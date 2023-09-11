fe3d3c87057d0b299b2fe9377c96fe75d1e7ff78
Pyry Heinoja <pyryheinoja1@kamk.fi>
2023-09-11 19:44:04 +0300

    started part 4

---
+++ osa4/backend/app.js
@@ -0,0 +1,33 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+
+app.use('/api/blogs', blogsRouter)
+
+
+
+
+// const Blog = mongoose.model('Blog', blogSchema)
+
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+
+
+
+
+module.exports = app;
--- part1_notes/backend/.eslintrc.js
+++ part1_notes/backend/.eslintrc.js
@@ -2,5 +2,11 @@ module.exports = {
   extends: ['airbnb', 'airbnb/hooks'],
   rules: {
     'no-console': 0,
+    'no-underscore-dangle': 0,
+    'no-param-reassign': 0,
+    'linebreak-style': 0,
+    'react/react-in-jsx-scope': 0,
+    'import/no-extraneous-dependencies': 0,
+    'no-alert': 0,
   },
 };
