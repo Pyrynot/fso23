9b9eece50a595802bbc31ecf47211ed823bf17a2
Pyry Heinoja <pyryheinoja1@kamk.fi>
2023-09-14 16:36:59 +0300

    part 4 notes app done

--- part1_notes/backend/controllers/notes.js
+++ part1_notes/backend/controllers/notes.js
@@ -1,9 +1,44 @@
 const notesRouter = require('express').Router();
+const jwt = require('jsonwebtoken');
 const Note = require('../models/note');
+const User = require('../models/user');
+
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization');
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '');
+  }
+  return null;
+};
 
 notesRouter.get('/', async (request, response) => {
-  const notes = await Note.find({});
+  const notes = await Note
+    .find({}).populate('user', { username: 1, name: 1 });
+
   response.json(notes);
+});
+
+notesRouter.post('/', async (request, response) => {
+  const { body } = request;
+
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' });
+  }
+
+  const user = await User.findById(decodedToken.id);
+
+  const note = new Note({
+    content: body.content,
+    important: body.important === undefined ? false : body.important,
+    user: user._id,
+  });
+
+  const savedNote = await note.save();
+  user.notes = user.notes.concat(savedNote._id);
+  await user.save();
+
+  response.json(savedNote);
 });
 
 notesRouter.get('/:id', async (request, response) => {
@@ -13,18 +48,6 @@ notesRouter.get('/:id', async (request, response) => {
   } else {
     response.status(404).end();
   }
-});
-
-notesRouter.post('/', async (request, response) => {
-  const { body } = request;
-
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-  });
-
-  const savedNote = await note.save();
-  response.status(201).json(savedNote);
 });
 
 notesRouter.delete('/:id', async (request, response) => {
--- part1_notes/backend/utils/middleware.js
+++ part1_notes/backend/utils/middleware.js
@@ -10,7 +10,6 @@ const requestLogger = (request, response, next) => {
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' });
-  return null;
 };
 
 const errorHandler = (error, request, response, next) => {
@@ -20,10 +19,17 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).send({ error: 'malformatted id' });
   } if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message });
+  } if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({
+      error: 'invalid token',
+    });
+  } if (error.name === 'TokenExpiredError') {
+    return response.status(401).json({
+      error: 'token expired',
+    });
   }
 
   next(error);
-  return null;
 };
 
 module.exports = {
